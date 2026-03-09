989d755cec1065593c30a701589ec3b884ecfe01
Riccardo Castellano <br97jrv2wf@privaterelay.appleid.com>
2026-03-09 23:28:06 +0100

    Bitchass

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import BitchAssDinosaur from './components/random-components/bitchassdinosaur.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BitchAssDinosaur />
   </StrictMode>,
 )
